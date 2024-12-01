09bf9907cf907b1e1d65e6ccbff46b5e7271f79d
Hannes <hanpie0501@gmail.com>
2024-12-01 16:36:27 +0100

    added code for handicap

--- handicap2021.py
+++ handicap2021.py
@@ -1,8 +1,58 @@
-def handicap():
+def handicap(games) -> list[float]:
+    """calculates the handicap given 0 to 20 games with the gross, net and stable ford value. 
+    Returns a list with 3 elements, where the first is the gross, the second is the net and
+    the third the stableford value"""
+
+    handicap = [54, 54, 54]  
+    numHoles = 0
+    for game in games:
+        numHoles += len[game] #shots is the array that tracks the amount of shots per hole
+    numGames = len(games)
+
+    if numHoles < 54:
+        return handicap
+
+    functions = [handicapDifferential, handicapDifferentialNet, handicapDifferentialStableford]
+
+    for i , func in enumerate(functions):
+        differentials = []
+        for game in games:
+            value: float = func(game)
+            differentials.append(value)
+        differentials.sort()
+        
+        if numGames == 3:
+            value = differentials[0]
+        elif numGames == 4:
+            value = differentials[0] - 1
+        elif numGames == 5:
+            value = differentials[0] - 2
+        elif numGames == 6 or numGames == 7:
+            average = (differentials[0] + differentials[1])/2
+            value = round(average - 1, 1)
+        elif numGames >= 8:
+            bestGames = differentials[0:7] #slice the best 8 games
+            average = sum(bestGames)/8
+            value = round(average, 1)
+        handicap[i] = value
+    
+    return handicap
+
+
+def handicapDifferential() -> float:
+    """calculates the handicap differential for one game"""
     print("a")
 
-def handicapNet():
+
+def handicapDifferentialNet() -> float:
+    """calculates the handicap differential for one game using net shots"""
     print("a")
 
-def handicapSF():
+
+def handicapDifferentialStableford() -> float:
+    """calculates the handicap differential for one game using the Stableford system"""
     print("a")
+
+
+shots = [5, 5 , 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5] #example shots with 18 entries
+games = [shots, shots, shots]
