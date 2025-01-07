7a16f13fa24ec2068315e2a6e32838c53ab0820a
Hannes <hanpie0501@gmail.com>
2025-01-07 08:49:20 +0100

    fixed the handicap calculation

--- packages/handicap2021.py
+++ packages/handicap2021.py
@@ -1,13 +1,17 @@
-def handicap(games: list[dict], courses: list[dict]) -> list[float]:
-    """calculates the handicap given 0 to 20 games with the gross, net and stable ford value. 
-    Also needs the course on which the game was played to work properly
-    Returns a list with 3 elements, where the first is the gross, the second is the net and
-    the third the stableford value"""
+def handicap(games: list[dict]) -> float:
+    """calculates the handicap given 0 to 20 games. 
+    
+    Args:
+    games (list[dict]): up to the last 20 games
+
+    Returns:
+    float: The handicap
+    """
 
     if not (len(games) <= 20):
         raise ValueError(f"Invalid parameter: len(games) = {len(games)}. You can only add a max of 20 games.")
 
-    handicap = [54, 54, 54]  
+    handicap = 54 
     numHoles = 0
     for game in games:
         numHoles += len[game["shots"]] #shots is the array that tracks the amount of shots per hole
@@ -16,46 +20,72 @@ def handicap(games: list[dict], courses: list[dict]) -> list[float]:
     if numHoles < 54:
         return handicap
 
-    functions = [handicapDifferential, handicapDifferentialNet, handicapDifferentialStableford]
-
-    for i , func in enumerate(functions):
-        differentials = []
-        for game in games:
-            course = [course for course in courses if course["courseID"] == game["courseID"]]
-            value: float = func(game, course[0])
-            differentials.append(value)
-        differentials.sort()
-        
-        if numGames == 3:
-            value = differentials[0]
-        elif numGames == 4:
-            value = differentials[0] - 1
-        elif numGames == 5:
-            value = differentials[0] - 2
-        elif numGames == 6 or numGames == 7:
-            average = (differentials[0] + differentials[1])/2
-            value = round(average - 1, 1)
-        elif numGames >= 8:
-            bestGames = differentials[0:7] #slice the best 8 games
-            average = sum(bestGames)/8
-            value = round(average, 1)
-        handicap[i] = value
+    
+    differentials = []
+    for game in games:
+        differentials.append(game["handicap_dif"])
+    differentials.sort()
+    
+    if numGames == 3:
+        value = differentials[0]
+    elif numGames == 4:
+        value = differentials[0] - 1
+    elif numGames == 5:
+        value = differentials[0] - 2
+    elif numGames == 6 or numGames == 7:
+        average = (differentials[0] + differentials[1])/2
+        value = round(average - 1, 1)
+    elif numGames >= 8:
+        bestGames = differentials[0:7] #slice the best 8 games
+        average = sum(bestGames)/8
+        value = round(average, 1)
+    handicap[i] = value
     
     return handicap
 
 
-def handicapDifferential() -> float:
-    """calculates the handicap differential for one game"""
+def handicapDifferential(game: dict, course: dict) -> float:
+    """
+    calculates the handicap differential for one game
+
+    Args:
+    game (dict): The game the calculation is being done on.
+    course (dict): The course corresponding to the game.
+
+    Returns:
+    float: gross handicap differential
+    """
+    shots = sum(game["shots"])
+    return ((shots - course["course_rating"]) * 113 / course["slope_rating"]) + game["pcc"]
+
+
+def handicapDifferentialNet(game: dict, course: dict) -> float:
+    """
+    calculates the handicap differential for one game using net shots.
+    Often used for more casual games can not be used for the Handicap without conversion
+
+    Args:
+    game (dict): The game the calculation is being done on.
+    course (dict): The course corresponding to the game.
+
+    Returns:
+    float: gross handicap differential
+    """
     print("a")
 
 
-def handicapDifferentialNet() -> float:
-    """calculates the handicap differential for one game using net shots"""
-    print("a")
+def handicapDifferentialStableford(game: dict, course: dict) -> float:
+    """
+    calculates the handicap differential for one game using the Stableford system
+    Often used for more casual games can not be used for the Handicap without conversion
 
+    Args:
+    game (dict): The game the calculation is being done on.
+    course (dict): The course corresponding to the game.
 
-def handicapDifferentialStableford() -> float:
-    """calculates the handicap differential for one game using the Stableford system"""
+    Returns:
+    float: gross handicap differential
+    """
     print("a")
 
 
@@ -63,3 +93,4 @@ def handicapDifferentialStableford() -> float:
 #TODO fill out the differential functions
 #TODO build packages
 #TODO create ui
+#TODO Soft- / Hardcap on handicap increase
