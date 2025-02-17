3220db2997d612cd823860842d873ce8bd3395fd
Hannes <hanpie0501@gmail.com>
2025-02-17 12:03:19 +0100

    added init

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-#for ui only I guess nad calls the data and handicap packages
+import datetime
 from packages.mauerDB  import MauerDB
+import os
 
-db = MauerDB("./db")
+# Define the database directory and file path
+db_dir = "./db"
+db_path = os.path.join(db_dir, "database.json")
+
+# Ensure the directory exists
+os.makedirs(db_dir, exist_ok=True)
+
+# Initialize the database
+db = MauerDB(db_path)
+
+# Initialize tables
 gamesTable = db.table("games") # stores the games that were already calculated
 courseTable = db.table("courses") # stores the courses
 cronTable = db.table("cron") # stores the games that are not in the calculation yet TODO maybe inconvenient to have games split for browsing
 hcLogTable = db.table("hcLog") # stores the old HCs max one per day
+
+# Insert base Handicap
+if len(hcLogTable) == 0:
+    gamesTable.insert({"ega": 54, "whs": 54, "date": datetime.datetime.now().isoformat()})
