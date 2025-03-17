03833eb9f8bf4e0fd6e21741694cadaa2857020a
Hannes <hanpie0501@gmail.com>
2025-03-17 17:08:29 +0100

    added debug buttons

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import json
+import pathlib
+import re
 import sys
 import matplotlib
 matplotlib.use('Qt5Agg')
@@ -53,7 +56,7 @@ class NeuesSpielDialog(QtWidgets.QDialog):
         button_box.accepted.connect(self.accept)
         button_box.rejected.connect(self.reject)
         layout.addWidget(button_box)
-
+  
         self.setLayout(layout)
 
         self.kurs_combo.currentIndexChanged.connect(self.update_schlagzahl_eingabe)
@@ -387,7 +390,8 @@ class HandicapUI(QtWidgets.QWidget):
         self.spiele_tabelle.setHorizontalHeaderLabels(["Datum", "Kurs", "Schläge"])
         layout.addWidget(self.spiele_tabelle)
 
-        self.setLayout(layout)
+        #UNCOMMENT AFTER DEBUG
+        #self.setLayout(layout)
 
         self.neues_spiel_button.clicked.connect(self.oeffne_neues_spiel_dialog)
         self.neuer_kurs_button.clicked.connect(self.neuer_kurs_hinzugefuegt)
@@ -395,6 +399,73 @@ class HandicapUI(QtWidgets.QWidget):
         self.export_scorecard_button.clicked.connect(self.oeffne_export_scorecard_dialog) #------------------------------------
 
         self.update()
+        #DEBUG CODE DELETE BEFORE SHIPPING
+##################################################################################
+        self.lade_games_button = QtWidgets.QPushButton("Import Lade Games", self)
+        self.lade_courses_button = QtWidgets.QPushButton("Import Lade Courses", self)
+        self.lade_games_button.clicked.connect(self.add_lade_games)
+        self.lade_courses_button.clicked.connect(self.add_lade_courses)
+
+        self.truncate_button = QtWidgets.QPushButton("Clear DB", self)
+        self.truncate_button.clicked.connect(self.truncate_db)
+
+        layout.addWidget(self.lade_courses_button)
+        layout.addWidget(self.lade_games_button)
+        layout.addWidget(self.truncate_button)
+
+        self.setLayout(layout)
+
+        self.update()
+
+
+    def truncate_db(self):
+        help.games.truncate()
+        help.courses.truncate()
+        help.hcLog.truncate()
+        self.update()
+
+    def add_lade_games(self):
+        files = list(pathlib.Path("./test/real_data/games/").glob("Runde*.json"))
+        files.sort(key=lambda f: int(re.search(r'\d+', f.stem).group()))
+        files = list(pathlib.Path("./test/real_data/games/").glob("Runde*.json"))
+        files.sort(key=lambda f: int(re.search(r'\d+', f.stem).group()))
+        games = list()
+        for file in files:
+            with file.open() as file:
+                data = json.load(file)
+            games.append(data)
+        
+        for game in games:
+            courseID = game["courseID"]
+            shots = game["shots"]
+            nineHole = game["is9Hole"]
+            pcc = game["pcc"]
+            cba = game["cba"]
+            gameDate = game["date"]
+            help.addGame(courseID, shots, nineHole, pcc, cba, gameDate)
+        self.update()
+
+    def add_lade_courses(self):
+        files = list(pathlib.Path("./test/real_data/courses/").glob("Kurs*.json"))
+        files = list(pathlib.Path("./test/real_data/courses/").glob("Kurs*.json"))
+        courses = list()
+        for file in files:
+            with file.open() as file:
+                data = json.load(file)
+            courses.append(data)
+        
+        for course in courses:
+            help.addCourse(
+                course["courseID"],
+                course["course_rating"],
+                course["slope_rating"],
+                course["par"],
+                course["handicap_stroke_index"],
+            )
+#################################################################################
+
+
+
 
     def oeffne_neues_spiel_dialog(self):
         kurse = help.getAllCourseIDs()
