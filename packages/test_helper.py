56487ae92f98f29290646d42dc94d25e4d22259d
Hannes <hanpie0501@gmail.com>
2025-03-17 08:54:53 +0100

    more readable test

--- packages/test_helper.py
+++ packages/test_helper.py
@@ -122,15 +122,14 @@ def test_addGame(
         helper.addGame(courseID, shots, nineHole, pcc, cba, gameDate)
 
         log = helper.hcLog.all()
-        log.sort(key=lambda doc: datetime.fromisoformat(doc["date"]))
-        lenLog = len(log)
+        log.sort(key=lambda doc: datetime.fromisoformat(doc["date"]), reverse=True)
 
         if index < 7:
-            #print(f"{index} EGA {game["game_id"]} {log[lenLog -1 ]["ega"]} == {EGA_handicap[index]}")
+            #print(f"{index} EGA {game["game_id"]} {log[0]["ega"]} == {EGA_handicap[index]}")
             
-            assert pytest.approx(log[lenLog -1]["ega"], rel=0.1) == EGA_handicap[index]
+            assert pytest.approx(log[0]["ega"], rel=0.1) == EGA_handicap[index]
         #if index >= 6:
-            #print(f"{index} WHS {game["game_id"]} {log[lenLog -1]["whs"]} == {WHS_handicap[index - 6]}")
+        print(f"{index} WHS {game["game_id"]} {log[0]["whs"]} == {WHS_handicap[index - 6]}")
             
             #assert log[lenLog -1]["whs"] == WHS_handicap[index - 6]
     
